27ab5f8294757c35cc48f1c3b66df2dfda828e5a
wsydt <2442157576@qq.com>
2020-07-23 13:40:37 +0800

    Test Commit

---
+++ src/test/java/com/example/wsy/WsyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wsy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
